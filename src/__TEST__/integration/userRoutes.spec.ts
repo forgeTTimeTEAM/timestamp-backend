73d91fb975dc1d4eba26e8bad8c67ad53f6a8450
alvseven <alv7contato@outlook.com>
2022-11-03 20:26:10 -0300

    refactor: creating tests archives and folders

--- src/__TEST__/integration/userRoutes.spec.ts
+++ src/__TEST__/integration/userRoutes.spec.ts
@@ -1,30 +1,30 @@
 import request from "supertest";
-import { prisma } from "../prisma/index";
-import { app } from "../server";
-
-const user = {
-  email: "yuran@example.com",
-  password: "password"
-}
+import { prisma } from "../../prisma";
+import { app } from "../../server";
 
 afterAll(async () => {
   await prisma.video_markers.deleteMany();
   await prisma.videos.deleteMany();
   await prisma.sprints.deleteMany();
+  await prisma.users_modules.deleteMany();
   await prisma.users.deleteMany();
   await prisma.modules.deleteMany();
-  await prisma.groups.deleteMany();
 });
+
+const user = {
+  email: "yuran@example.com",
+  password: "password",
+};
 
 describe("routes - users/", () => {
   test("should create a user", async () => {
-    const user = {
+    const modules = await prisma.modules.findMany();
+    const createUser = {
+      ...user,
       name: "yuran",
-      email: "yuran@example.com",
-      password: "password",
-      groupId: null,
+      modulesId: modules,
     };
-    const res = await request(app).post("/users").send(userCreate);
+    const res = await request(app).post("/users").send(createUser);
 
     expect(res.status).toBe(201);
     expect(res.body).toHaveProperty("name");
@@ -41,34 +41,38 @@ describe("routes - users/", () => {
   test("should be able to return password error", async () => {
     const userInvalidPassword = {
       ...user,
-      password: "password12"
-    }
-    const res = await request(app).post("/users/login").send(userInvalidPassword);
+      password: "password12",
+    };
+    const res = await request(app)
+      .post("/users/login")
+      .send(userInvalidPassword);
 
     expect(res.status).toBe(403);
     expect(res.body).toHaveProperty("message");
-  })
+  });
 
   test("should be able to return error when logging in with wrong email", async () => {
     const userInvalidEmail = {
       ...user,
       email: "yursan@example.com",
-    }
+    };
     const res = await request(app).post("/users/login").send(userInvalidEmail);
-    
+
     expect(res.status).toBe(404);
     expect(res.body).toHaveProperty("message");
-  })
+  });
 
   test("should be able to return error when logging in without email and password", async () => {
     const usernameWithoutPasswordAndEmail = {
       email: "",
-      password: ""
-    }
+      password: "",
+    };
 
-    const res = await request(app).post("/users/login").send(usernameWithoutPasswordAndEmail);
-    
+    const res = await request(app)
+      .post("/users/login")
+      .send(usernameWithoutPasswordAndEmail);
+
     expect(res.status).toBe(403);
     expect(res.body).toHaveProperty("message");
-  })
+  });
 });
