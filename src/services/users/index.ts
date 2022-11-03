41c6f2f799990e565ed6da07b9a3fcf2afa2bc4d
alvseven <alv7contato@outlook.com>
2022-11-03 17:25:00 -0300

    refactor: changing create user and find users routes

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -1,2 +1,3 @@
 export { createUserService } from "./createUser.service";
 export { findUsersByGroupService } from "./findUsersByGroup.service";
+export { getUserProfileService } from "./getUserProfile.service";
