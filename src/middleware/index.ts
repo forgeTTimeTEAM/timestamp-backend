a48b61a205dccad7ae64d59c45d4d97d6b9a9cb8
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-11-03 12:36:54 -0300

    feat: creating verify adm permission

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,2 +1,3 @@
 export { errorMiddleware } from "./errors.middleware";
 export { verifyTokenMiddleware } from "./verifyToken.middleware";
+export { verifyAdmPermissionMiddleware } from "./verifyAdmPermission.middleware";
--- src/middleware/verifyToken.middleware.ts
+++ src/middleware/verifyToken.middleware.ts
@@ -37,7 +37,7 @@ const verifyTokenMiddleware: RequestHandler = (req, res, next) => {
     }
   );
 
-  next();
+  return next();
 };
 
 export { verifyTokenMiddleware };
