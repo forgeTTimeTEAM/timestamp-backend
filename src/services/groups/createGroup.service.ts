c3851277635bea177c43fdc3bcad7946f0952744
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-11-04 17:07:09 -0300

    fix: changing module and sprint name to opcional

--- src/services/groups/createGroup.service.ts
+++ src/services/groups/createGroup.service.ts
@@ -2,33 +2,33 @@ import { IGroupRequest } from "../../interfaces/groups";
 import { prisma } from "../../prisma";
 
 const createGroupService = async ({
-    moduleName,
-    sprintName,
+  modulePrefixName = "module",
+  sprintPrefixName = "sprint",
 }: IGroupRequest) => {
-    const createSprint = (_: null, index: number) => ({
-        name: `${sprintName} ${index + 1}`,
-    });
-    const sprintsToCreate = new Array(8).fill(null).map(createSprint);
-    const sprints = { create: sprintsToCreate };
+  const createSprint = (_: null, index: number) => ({
+    name: `${sprintPrefixName} ${index + 1}`,
+  });
+  const sprintsToCreate = new Array(8).fill(null).map(createSprint);
+  const sprints = { create: sprintsToCreate };
 
-    const moduleToCreate = {
-        name: `${moduleName} 1`,
-        sprints,
-    };
-    const modules = { create: moduleToCreate };
+  const moduleToCreate = {
+    name: `${modulePrefixName} 1`,
+    sprints,
+  };
+  const modules = { create: moduleToCreate };
 
-    const data = { modules };
-    const include = {
-        modules: {
-            include: {
-                sprints: true,
-            },
-        },
-    };
+  const data = { modules };
+  const include = {
+    modules: {
+      include: {
+        sprints: true,
+      },
+    },
+  };
 
-    const createdGroup = await prisma.groups.create({ data, include });
+  const createdGroup = await prisma.groups.create({ data, include });
 
-    return createdGroup;
+  return createdGroup;
 };
 
 export { createGroupService };
