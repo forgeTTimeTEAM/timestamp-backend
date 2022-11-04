da7c9eb8f7bb515cd0b21d27029692d5ed5d622f
Sara Lins <saralins19@gmail.com>
2022-11-04 19:16:21 -0300

    fix: change name route modules

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { errorMiddleware } from "../middleware/errors.middleware";
 import { groupsRouter } from "./groups.routes";
+import { moduleRoutes } from "./modules.routes";
 import { usersRouter } from "./users.routes";
 
 const router = Router();
 
 router.use("/users", usersRouter);
 router.use("/groups", groupsRouter);
+router.use("/module", moduleRoutes);
 router.use(errorMiddleware);
 
 export { router };
