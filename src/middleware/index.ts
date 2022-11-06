add7984c55333c69a17fde7e4c283346f617416e
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-11-06 04:59:21 -0300

    feat: adding schema validation middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,3 +1,4 @@
 export { errorMiddleware } from "./errors.middleware";
 export { verifyTokenMiddleware } from "./verifyToken.middleware";
 export { verifyAdmPermissionMiddleware } from "./verifyAdmPermission.middleware";
+export { validateSchemaMiddleware } from "./validateSchema.middleware";
