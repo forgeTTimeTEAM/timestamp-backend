a89c0f388e69bc29c930cd570e178b5c7c1c2f0a
mathbrasil <matheusbrasilpvh@hotmail.com>
2022-11-09 21:04:23 -0400

    fix: fixing readme structure

--- src/controllers/markers/createMarker.controller.ts
+++ src/controllers/markers/createMarker.controller.ts
@@ -1,13 +1,12 @@
 import { RequestHandler } from "express";
 
-import { createMarker } from "../../services/markers/index";
+import { createMarkerServices } from "../../services/markers/index";
 
 const createMarkerController: RequestHandler = async (req, res) => {
   const { marks, videoId } = req.body;
   const groupId = req.user.groupId;
 
-  const createdMarker = await createMarker({ marks, videoId, groupId });
-
+  const createdMarker = await createMarkerServices({ marks, videoId, groupId });
   return res.status(201).json(createdMarker);
 };
 
--- src/services/markers/updateMarker.service.ts
+++ src/services/markers/updateMarker.service.ts
@@ -1,9 +1,9 @@
 import { prisma } from "../../prisma";
 
 import { AppError } from "../../errors/AppError";
-import { IVideoMarkerUpdade } from "../../interfaces/videoMarker";
+import { IUserService, IVideoMarkerUpdade } from "../../interfaces/videoMarker";
 
-const updateMarkerService = async ({ id, bodyPatch }: IVideoMarkerUpdade, idUser: string) => {
+const updateMarkerService = async ({ id, bodyPatch }: IVideoMarkerUpdade, userReq: IUserService) => {
   const marker = await prisma.video_markers.findFirst({
     where: {
       id
@@ -37,10 +37,10 @@ const updateMarkerService = async ({ id, bodyPatch }: IVideoMarkerUpdade, idUser
   if (!marker) throw new AppError("Marker not found", 404);
 
   const isInstructor = marker.video.sprint.module.group.users.find(
-    (user) => user.id === idUser
+    (user) => userReq.id === user.id
   );
 
-  if (!isInstructor)
+  if (!isInstructor && userReq.role === "INSTRUCTOR")
     throw new AppError("Instructor does not own this mark", 401);
 
   if(marker && bodyPatch.time && marker?.video.video_markers.find((videoMarker => videoMarker.time.localeCompare(bodyPatch.time!) === 0))) {
