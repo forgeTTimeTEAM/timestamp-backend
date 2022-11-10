fe1e7890d44ee253dacb69421ac6b504051c744e
Yuran Vita- <yuranxd1@gmail.com>
2022-11-09 21:47:35 -0400

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import "dotenv/config";
-
+import cors from "cors";
 import express from "express";
 import "express-async-errors";
 
 import { router } from "./routes";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
